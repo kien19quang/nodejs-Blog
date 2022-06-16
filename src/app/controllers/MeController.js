ee70097cd8cecd63e1421cee37b650f5f0f187e9
Quang Kiên <kien19quang@gmail.com>
2022-06-16 22:54:38 +0700

    video 39 nodejs

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -9,7 +9,15 @@ class NewsController {
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
 
-        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+        let courseQuery = Course.find({})
+        if (res.locals._sort.enabled) {
+            courseQuery = courseQuery.sort({
+                [res.locals._sort.column]: res.locals._sort.type
+            })
+        }
+
+
+        Promise.all([courseQuery, Course.countDocumentsDeleted()])
             .then(([courses, deletedCount]) =>
                 res.render('me/store-courses', {
                     deletedCount: deletedCount ? deletedCount : 0,
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const methodOverride = require('method-override')
 const path = require('path');
 const route = require('./routes/web');
 const db = require('./config/db')
-
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
 
 // Connect to DB
 db.connect()
@@ -28,11 +28,34 @@ app.use(express.json())
 
 app.use(methodOverride('_method'))
 
+app.use(SortMiddleware);
+
 // Template engine
 app.engine('hbs', engine({
     extname: '.hbs',
     helpers: {
-        sum: (a, b) => a + b
+        sum: (a, b) => a + b,
+        sortable: (field, sort) => {
+            const sortType = field === sort.column ? sort.type : 'default'
+
+            const icons = {
+                default: 'oi oi-elevator',
+                asc: 'oi oi-sort-ascending',
+                desc: 'oi oi-sort-descending'
+            }
+            const types = {
+                default: 'desc',
+                asc: 'desc',
+                desc: 'asc'
+            }
+
+            const icon = icons[sortType]
+            const type = types[sortType]
+
+            return `<a href="?_sort&column=${field}&type=${type}">
+                        <span class="${icon}"></span>
+                    </a>`
+        }
     }
 }));
 app.set('view engine', 'hbs');
